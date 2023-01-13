82644b9b504d91e0fb4e6751c3b21ed38ad892d5
Sgreen <Sgreensneh@gmail.com>
2023-01-13 14:03:46 +0100

    third commit

---
+++ src/Chaptersix/Divisible.java
@@ -0,0 +1,32 @@
+package chapterSix;
+
+import java.util.Scanner;
+
+public class Divisible {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Enter an integer: ");
+            int userInput = input.nextInt();
+
+            if(isDivisible(userInput)){
+                System.out.println(userInput + " is divisible by 5");
+            }
+            else {
+                System.out.println(userInput + " is not divisible by 5");
+            }
+
+        }
+
+
+    }
+    public static boolean isDivisible(int divisible){
+        if(divisible % 5 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
